0a6a75485a12b8c9d03d48bff642a35a8f6b00a7
kazuya kawaguchi <kawakazu80@gmail.com>
2012-10-18 17:03:55 +0900

    add tests

--- test/test.kernel.js
+++ test/test.kernel.js
@@ -3,6 +3,7 @@
 var should = require('should');
 var assert = require('assert');
 var format = require('util').format;
+var fs = require('fs');
 var kernel = require('../lib/kvs').kernel;
 
 
@@ -17,7 +18,6 @@ var itShouldBeDefined = function (obj, prop) {
 describe('kernel', function () {
   describe('mmap', function () {
     var mmap = kernel.mmap;
-
     itShouldBeDefined(mmap, 'PROT_NONE');
     itShouldBeDefined(mmap, 'PROT_READ');
     itShouldBeDefined(mmap, 'PROT_WRITE');
@@ -38,5 +38,37 @@ describe('kernel', function () {
     //itShouldBeDefined(mmap, 'MAP_UNINITIALIZED');
     itShouldBeDefined(mmap, 'PAGESIZE');
 
+    describe('map', function () {
+      describe('arguments', function () {
+        var fd, size;
+        before(function (done) {
+          fd = fs.openSync(__filename, 'r');
+          size = fs.fstatSync(fd).size;
+          done();
+        });
+
+        // TODO: should be check argument pattern ...
+        it('should be succeed with `all` arguments', function (done) {
+          var buf = mmap.map(size, mmap.PROT_READ, mmap.MAP_SHARED, fd, 0);
+          buf.length.should.eql(size);
+          done();
+        });
+        it('should be succeed with `4` arguments', function (done) {
+          var buf = mmap.map(size, mmap.PROT_READ, mmap.MAP_SHARED, fd);
+          buf.length.should.eql(size);
+          done();
+        });
+        it('should be succeed with arguments and callback', function (done) {
+          mmap.map(size, mmap.PROT_READ, mmap.MAP_SHARED, fd, 0, function (err, buf) {
+            if (err) { return done(err); }
+            buf.length.should.eql(size);
+            done();
+          });
+        });
+      });
+
+      describe('large file size', function () {
+      });
+    });
   });
 });
